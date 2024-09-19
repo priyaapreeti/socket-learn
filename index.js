fd04c50204a94191322cc6679fa527fea80b9e4b
codesofpreeti <preetiipriyaa@gmail.com>
2024-09-19 10:41:18 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const serve = http.createServer(app);
+
+const io = new Server(serve, {
+  cors: { origin: "*", methods: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+  console.log(socket.id);
+  socket.on("disconnet",()=>{console.log("user disconnected!")})
+});
+
+serve.listen(3000, () => console.log("Listening on 3000"));
